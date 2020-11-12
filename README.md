797e04fe64792973fbfbf1129417d8c997634ab2
Erik Turøy Midtun <eriktm@stud.ntnu.no>
2020-11-12 12:45:21 +0100

    added Readme and changed a color in fish prompt

---
+++ README.md
@@ -0,0 +1,5 @@
+Set fish_prompt styling with git branch and colored arrows:
+
+```bash
+cat fish_prompt.fish > ~/.config/fish/functions/fish_prompt.fish
+```
---
+++ fish_prompt.fish
@@ -0,0 +1,15 @@
+function fish_prompt
+    test $SSH_TTY
+    and printf (set_color red)$USER(set_color brwhite)'@'(set_color yellow)(prompt_hostname)' '
+    test "$USER" = 'root'
+    and echo (set_color red)"#"
+    set_color normal
+    # https://stackoverflow.com/questions/24581793/ps1-prompt-in-fish-friendly-interactive-shell-show-git-branch
+    set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
+    set_color purple
+    echo -n "$git_branch "
+    echo -n (set_color blue)(prompt_pwd)
+     # Main
+    echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+end
+
